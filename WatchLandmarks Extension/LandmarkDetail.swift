caf85674c4a4bbe699925d20dab081b6a0c8b2e4
Muzahidul Islam <>
2021-01-22 15:30:22 +0600

    Landmark WatchOS App completed

---
+++ WatchLandmarks Extension/LandmarkDetail.swift
@@ -0,0 +1,66 @@
+//
+//  LandmarkDetail.swift
+//  WatchLandmarks Extension
+//
+//  Created by Muzahidul on 22/1/21.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
+    var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { landmark.id == $0.id })!
+    }
+    var body: some View {
+        ScrollView {
+            VStack {
+                CircleImage(image: landmark.image.resizable())
+                    .scaledToFit()
+                
+                Text(landmark.name)
+                
+                Toggle(isOn: $modelData.landmarks[landmarkIndex].isFavorite) {
+                    Text("Favorite")
+                }
+                
+                Divider()
+                
+                Text(landmark.park)
+                    .font(.caption)
+                    .bold()
+                    .lineLimit(0)
+                
+                Text(landmark.state)
+                    .font(.caption)
+                
+                Divider()
+                
+                MapView(coordinate: landmark.locationCoordinate)
+                    .scaledToFit()
+            }
+            .padding(16)
+        }
+        .navigationTitle("Landmarks")
+    }
+}
+
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var modelData = ModelData()
+    static var previews: some View {
+        Group {
+            LandmarkDetail(landmark: modelData.landmarks[0])
+                .environmentObject(modelData)
+                .previewDevice("Apple Watch Series 5 - 44mm")
+            
+            LandmarkDetail(landmark: modelData.landmarks[0])
+                .environmentObject(modelData)
+                .previewDevice("Apple Watch Series 5 - 40mm")
+        }
+        
+            
+    }
+}
