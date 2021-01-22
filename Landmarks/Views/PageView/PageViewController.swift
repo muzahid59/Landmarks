95fa6475eba506760adf3527b2d20baa80f92184
Muzahidul Islam <>
2021-01-22 11:32:35 +0600

    Page control update by swiping pager manually

--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -21,7 +21,8 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
         pageViewController.dataSource = context.coordinator
-        
+        pageViewController.delegate = context.coordinator
+
         return pageViewController
     }
     
